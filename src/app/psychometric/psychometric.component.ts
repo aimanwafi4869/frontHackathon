11070c304d7c3ea91b8a9e3c112ea852569187b6
Aiman <aimanwafi3991@yahoo.com>
2024-12-05 23:04:03 +0800

    psychometric and proxy

--- src/app/psychometric/psychometric.component.ts
+++ src/app/psychometric/psychometric.component.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import Swal from 'sweetalert2'
+
 @Component({
   selector: 'app-psychometric',
   standalone: true,
@@ -21,33 +23,25 @@ export class PsychometricComponent {
     'bool5': [],
     'bool6': [],
   }
-  dimension = {
-    1:0,
-    2:0,
-    3:0,
-    4:0,
-    5:0,
-    6:0
-  }
   result = {
-    'bm': undefined,
-    'bi': undefined,
-    'maths': undefined,
-    'sej': undefined,
-    'science': undefined,
-    'pi': undefined,
-    'pm': undefined,
-    'addmath': undefined,
-    'physics': undefined,
-    'chemistry': undefined,
-    'biology': undefined,
-    'cs': undefined,
-    'akaun': undefined,
-    'eko': undefined,
-    'perniagaan': undefined,
-    'geo': undefined,
-    'sainssukan': undefined,
-    'psv': undefined
+    'bm': 0,
+    'bi': 0,
+    'maths': 0,
+    'sej': 0,
+    'science': 0,
+    'pi': 0,
+    'pm': 0,
+    'addmath': 0,
+    'physics': 0,
+    'chemistry': 0,
+    'biology': 0,
+    'cs': 0,
+    'akaun': 0,
+    'eko': 0,
+    'perniagaan': 0,
+    'geo': 0,
+    'sainssukan': 0,
+    'psv': 0
   }
 
   constructor(protected http: HttpClient){}
@@ -63,20 +57,109 @@ export class PsychometricComponent {
     //console.log("Total score from boolean values:", total);
   }
 
-  introvert(value: number): void{
-    this.dimension[1]+= value
-
-  }
-  extrovert(e:any ,value: number): void{
-    this.dimension[1] -= value
-  }
-
   predict():void{
-    const data = {"param":[[1,0,1,1,0,3,2,1,1,1,0,1,1,1,1,1,0,0,0,0,0,0,0]]}
+    let dimension1 = 0
+    let totalDimension1 = 0
+    for(let i = 0; i < this.boolean.bool1.length; i++){
+      totalDimension1 += this.boolean.bool1[i]
+    }
+
+    if(totalDimension1 >= 0){
+      dimension1 = 1
+    }else{
+      dimension1 = 0
+    }
+
+    let dimension2 = 0
+    let totalDimension2 = 0
+    for(let i = 0; i < this.boolean.bool2.length; i++){
+      totalDimension2 += this.boolean.bool2[i]
+    }
+
+    if(totalDimension2 >= 0){
+      dimension2= 1
+    }else{
+      dimension2 = 0
+    }
+
+    let dimension3 = 0
+    let totalDimension3 = 0
+    for(let i = 0; i < this.boolean.bool3.length; i++){
+      totalDimension3 += this.boolean.bool3[i]
+    }
+
+    if(totalDimension3 >= 0){
+      dimension3= 1
+    }else{
+      dimension3 = 0
+    }
+
+    let dimension4 = 0
+    let totalDimension4 = 0
+    for(let i = 0; i < this.boolean.bool4.length; i++){
+      totalDimension4 += this.boolean.bool4[i]
+    }
+
+    if(totalDimension4 >= 0){
+      dimension4= 1
+    }else{
+      dimension4 = 0
+    }
+
+    let dimension5 = 0
+    let totalDimension5 = 0
+    for(let i = 0; i < this.boolean.bool5.length; i++){
+      totalDimension5 += this.boolean.bool5[i]
+    }
+
+    if(totalDimension5 >= 0){
+      dimension5= 1
+    }else{
+      dimension5= 0
+    }
+
+    let dimension6 = 0
+    let totalDimension6 = 0
+    for(let i = 0; i < this.boolean.bool6.length; i++){
+      totalDimension6 += this.boolean.bool6[i]
+    }
+
+    if(totalDimension6 >= 0){
+      dimension6= 1
+    }else{
+      dimension6 = 0
+    }
+
+    const data = {"param":[[dimension1,dimension2,dimension3,dimension4,dimension5,dimension6,
+      Number(this.result.bm),
+      Number(this.result.bi),
+      Number(this.result.maths),
+      Number(this.result.sej),
+      Number(this.result.science),
+      Number(this.result.pi),
+      Number(this.result.pm),
+      Number(this.result.addmath),
+      Number(this.result.physics),
+      Number(this.result.chemistry),
+      Number(this.result.biology),
+      Number(this.result.cs),
+      Number(this.result.akaun),
+      Number(this.result.perniagaan),
+      Number(this.result.geo),
+      Number(this.result.sainssukan),
+      Number(this.result.psv)]]}
     const req = this.http.post<any>('/api/ai/sklearn/predict',data,{
       headers: {'Content-Type':'application/json; charset=utf-8'}
     });
-    const resp = req.subscribe()
-    console.log(resp)
+    req.subscribe(res=>{
+      Swal.fire({
+        title: 'Field of Study Recommendation',
+        text: res.prediction,
+        icon: 'success',
+        confirmButtonText: 'OK'
+      })
+      console.log(res.prediction)
+    })
+    // console.log(resp)
   }
 }
